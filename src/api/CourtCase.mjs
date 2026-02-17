3095199a416a0b3376ac1166749d7d2c25362d99
Erik Itter <erik.itter@gmail.com>
2026-02-17 16:01:51 +0100

    brainstorming

--- src/api/CourtCase.mjs
+++ src/api/CourtCase.mjs
@@ -1,22 +1,62 @@
+import { Document, DocumentData } from "./Document.mjs";
+
+VERSION = '0.0.0.1';
+
 class CourtCaseData {
 
+    #version = VERSION;
+
+}
+
+class CaseFiling extends Document {
+
+    #version = VERSION;
+
+    constructor(data, options = {}) {
+        super(data);
+
+        // TODO: apply options
+
+        this.received = new Date();
+    }
 }
 
 class CourtCase {
 
+    #documents;
+    #decision;
+    #version = VERSION;
+
     DECISSIONS = {
         'dismissed': {
-            label: 'Dismissed',
+            label: 'dismissed',
+            desciption: ``
+        },
+        'sumbitted': {
+            label: 'submitted',
+            description: ``
+        },
+        'concluded': {
+            label: 'concluded',
             desciption: ``
         }
     };
 
     constructor(data) {
-        this.documents = [];
+        this.documents = new Map();
         this.decission = undefined;
     }
 
     isActive() {
         return !this.decission;
+    }
+
+    addDocument(document) {
+    }
+
+    getDocument(id) {
+    }
+
+    filterDocuments(f) {
     }
 }
